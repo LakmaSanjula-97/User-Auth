c62747723f646255dbb7d402b9f393b70454a3ba
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-11-08 01:02:32 +0530

    add server file

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/user.routes");
+const { notFound , errorHandler} = require("./middleware/errorMiddleware");
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is Running");
+});
+
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+// app.get('/api/chat', (req, res) => {
+//     res.send(chats);
+// });
+ 
+// app.get('/api/chat/:id', (req, res) => {
+//     const singleChat = chats.find((c) => c._id === req.params.id);
+//     res.send(singleChat);
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`server is running on PORT ${PORT}`));
