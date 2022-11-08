c6c6e97601101827b772db1a97485dd72559ff25
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-11-08 09:59:43 +0530

    additional pages

---
+++ frontend/src/Components/Chat/ChatPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import axios from 'axios';
+// import { useEffect, useState } from "react";
+
+const ChatPage = () => {
+
+    // const [chats, setChats] = useState([]);
+
+    // const featchChats = async () => {
+    //     const {data} = await axios.get("/api/chat");
+
+    //     setChats(data);
+    // };
+
+    // useEffect(() => {
+    //     featchChats();
+    // }, [])
+
+  return (
+    <div>
+        {/* { chats.map((chat) => (
+            <div key = { chat._id }>{ chat.chatName }</div>
+        ))} */}
+          
+chatata
+    </div>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/Components/Home/HomePage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { 
+  Container, 
+  Box, 
+  Text,
+  Tab, 
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel
+} from "@chakra-ui/react";
+import Login from '../Auth/Login';
+import SignUp from '../Auth/SignUp';
+
+const HomePage = () => {
+  return (
+    <Container maxW='xl' centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p = {3}
+        bg = {'white'}
+        w = "100%"
+        m = "40px 0 15px 0"
+        borderRadius= "lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontFamily= "work sans"
+          fontSize= "4xl"
+          color= "black"
+        >
+          Chat Application
+        </Text>
+      </Box>
+      <Box
+        p = {4}
+        bg = {'white'}
+        w = "100%"
+        color = "black"
+        borderRadius= "lg"
+        borderWidth="1px"
+      >
+        <Tabs 
+          variant='soft-rounded' 
+          colorScheme='green'
+        >
+          <TabList>
+            <Tab width="50%">Sign-In</Tab>
+            <Tab width="50%">Sign-Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  )
+}
+
+export default HomePage
