9a36f42737edc1616fc8517098a118a7f341da67
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-11-12 23:47:42 +0530

    logout

--- frontend/src/Components/NavigatioBar/NavigationBar.js
+++ frontend/src/Components/NavigatioBar/NavigationBar.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import { Container, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-const NavigationBar = () => {
-  return (
-    <Container>
-      <Button>
-        <Link to="/create_member">Add Staff Members</Link>
-      </Button>
-    </Container>
-  );
+function NavigationBar() {
+  
+  const history = useHistory();
+
+      
+  
+    async function logout(e){
+      try {
+          
+
+            e.preventDefault();
+
+            
+        const userInfo = localStorage.getItem("userInfo")
+          ? JSON.parse(localStorage.getItem("userInfo"))
+          : null;
+        const config = {
+          headers: {
+            Authorization: `Bearer ${userInfo.token}`,
+          },
+        };
+              
+        const { data } = await axios.get("/api/staff/logout", config);
+        console.log(data);
+        history.push("/");
+        console.log("uhuh", data); 
+        }catch(err){
+
+        }
+    }
+    return(
+
+       
+        <div id="view1">
+  
+            <Button id="view2" onClick={(e)=>{logout(e)}}><a>Logout</a></Button>
+        </div>
+        
+    )
 }
+export default NavigationBar;
 
-export default NavigationBar
