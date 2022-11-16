e1c2b10c5cb5936545fd10319c48eb256976a123
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2022-11-16 18:00:30 +0530

    update the navigationbar

--- frontend/src/Components/File/FileUploadPage.js
+++ frontend/src/Components/File/FileUploadPage.js
@@ -8,46 +8,51 @@ import {
   TabPanel,
 } from "@chakra-ui/react";
 import FileUpload from "./FileUpload";
+import NavigationBar from '../NavigatioBar/NavigationBar';
 
 const FileUploadPage = () => {
   const userInfo = localStorage.getItem("userInfo")?JSON.parse(localStorage.getItem('userInfo')):null
 
   return (
     <>
-    { userInfo.role === "manager"?(
-    <Container maxW="xl" centerContent>
-      <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg={"white"}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text fontFamily="work sans" fontSize="4xl" color="black">
-          Uplaod File Here
-        </Text>
-      </Box>
-      <Box
-        p={4}
-        bg={"white"}
-        w="100%"
-        color="black"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Tabs variant="soft-rounded" colorScheme="green">
-          <TabPanels>
-            <TabPanel>
-              <FileUpload />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
-    ):(<h1>ffff</h1>)}
+      {userInfo.role === "manager" ? (
+        <Container maxW="xl" centerContent>
+          <NavigationBar />
+          <Box
+            d="flex"
+            justifyContent="center"
+            p={3}
+            bg={"white"}
+            w="100%"
+            m="40px 0 15px 0"
+            borderRadius="lg"
+            borderWidth="1px"
+            marginTop={40}
+          >
+            <Text fontFamily="work sans" fontSize="4xl" color="black">
+              Uplaod File Here
+            </Text>
+          </Box>
+          <Box
+            p={4}
+            bg={"white"}
+            w="100%"
+            color="black"
+            borderRadius="lg"
+            borderWidth="1px"
+          >
+            <Tabs variant="soft-rounded" colorScheme="green">
+              <TabPanels>
+                <TabPanel>
+                  <FileUpload />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </Container>
+      ) : (
+        <h1>Not Authorized. Please login or signup to the system</h1>
+      )}
     </>
   );
 }
--- frontend/src/Components/Messages/SaveMessagePage.js
+++ frontend/src/Components/Messages/SaveMessagePage.js
@@ -8,7 +8,7 @@ import {
   TabPanel,
 } from "@chakra-ui/react";
 import Message from "./SaveMessage";
-
+import NavigationBar from '../NavigatioBar/NavigationBar';
 
 
 const SaveMessagePage = () => {
@@ -16,41 +16,45 @@ const SaveMessagePage = () => {
 
   return (
     <>
-    { userInfo.role === "manager" || userInfo.role === "worker"?(
-    <Container maxW="xl" centerContent>
-      <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg={"white"}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text fontFamily="work sans" fontSize="4xl" color="black">
-          Message
-        </Text>
-      </Box>
-      <Box
-        p={4}
-        bg={"white"}
-        w="100%"
-        color="black"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Tabs variant="soft-rounded" colorScheme="green">
-          <TabPanels>
-            <TabPanel>
-              <Message />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
-     ):(<h1>ffff</h1>)}
-     </>
+      {userInfo.role === "manager" || userInfo.role === "worker" ? (
+        <Container maxW="xl" centerContent>
+          <NavigationBar />
+          <Box
+            d="flex"
+            justifyContent="center"
+            p={3}
+            bg={"white"}
+            w="100%"
+            m="40px 0 15px 0"
+            borderRadius="lg"
+            borderWidth="1px"
+            marginTop={40}
+          >
+            <Text fontFamily="work sans" fontSize="4xl" color="black">
+              Message
+            </Text>
+          </Box>
+          <Box
+            p={4}
+            bg={"white"}
+            w="100%"
+            color="black"
+            borderRadius="lg"
+            borderWidth="1px"
+          >
+            <Tabs variant="soft-rounded" colorScheme="green">
+              <TabPanels>
+                <TabPanel>
+                  <Message />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </Container>
+      ) : (
+        <h1>Not Authorized. Please login or signup to the system</h1>
+      )}
+    </>
   );
 }
 
