9078ecf0fac71bc635b24bba5e818f75c085499c
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2022-11-17 12:11:32 +0530

    file model

--- Backend/models/File.Model.js
+++ Backend/models/File.Model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema(
@@ -9,20 +9,30 @@ const fileSchema = new Schema(
     },
     title: { type: String, required: true },
     doc: { type: String, required: true },
+    fileSecurityKey : {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fileInitVector : {
+      type: String,
+      required: true, 
+      trim: true,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-fileSchema.pre("save", async function (next) {
-  if (!this.isModified) {
-    next();
-  }
+// fileSchema.pre("save", async function (next) {
+//   if (!this.isModified) {
+//     next();
+//   }
 
-  const salt = await bcrypt.genSalt(10);
-  this.doc = await bcrypt.hash(this.doc, salt);
-});
+//   const salt = await bcrypt.genSalt(10);
+//   this.doc = await bcrypt.hash(this.doc, salt);
+// });
 
 //mongodb data table name
 const File = mongoose.model("File", fileSchema);
