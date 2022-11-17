f5828c9aada15a7eb02f8a535b921f4d4995942d
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2022-11-17 12:11:42 +0530

    message model

--- Backend/models/Message.Model.js
+++ Backend/models/Message.Model.js
@@ -10,6 +10,16 @@ const messageSchema = new Schema(
     writerName: { type: String, required: true },
     date: { type: String, required: true },
     description: { type: String, required: true },
+    messageSecurityKey : {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    messageInitVector : {
+      type: String,
+      required: true, 
+      trim: true,
+    },
   },
   {
     timestamps: true,
